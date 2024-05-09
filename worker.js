83a80bda03daedc25755ae9b36d1c8e0b53d5ab6
hypackelnetwork <168493576+hypackelnetwork@users.noreply.github.com>
2024-05-09 13:18:32 +0000

    nahhhh

---
+++ worker.js
@@ -0,0 +1,7 @@
+// Used to be something
+// You didn't have to cut me off
+// Make it like we were nothing
+// i dont even need your love
+// you treat me like a stranger and i feel so 
+// no you didnt have to stoop so low
+// have you friends collect your records and change your number
